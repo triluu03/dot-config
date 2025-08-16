e86587c18f1fb154a9a258d0dc094ceb94a02b86
Tri D Luu <91616223+triluu03@users.noreply.github.com>
2025-08-16 10:59:48 +0300

    Update nvim config

--- nvim/lua/kickstart/plugins/autopairs.lua
+++ nvim/lua/kickstart/plugins/autopairs.lua
@@ -1,8 +1,7 @@
 -- autopairs
 -- https://github.com/windwp/nvim-autopairs
-
 return {
-  'windwp/nvim-autopairs',
-  event = 'InsertEnter',
-  opts = {},
+	"windwp/nvim-autopairs",
+	event = "InsertEnter",
+	opts = {},
 }
