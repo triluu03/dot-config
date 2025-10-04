09d92d41a720edd526b2cbe2d124814dd49b1925
Tri D Luu <91616223+triluu03@users.noreply.github.com>
2025-10-04 22:57:05 +0300

    nvim: remove the debugger configuration for gui2 code

--- nvim/lua/custom/plugins/debug.lua
+++ nvim/lua/custom/plugins/debug.lua
@@ -82,31 +82,5 @@ return {
 		-- Install golang specific config
 		-- require('dap-go').setup()
 		require("dap-python").setup("uv")
-
-		-- gui2 debugger setup
-		-- require("dap-vscode-js").setup({
-		-- 	-- node_path = "node", -- Path of node executable. Defaults to $NODE_PATH, and then "node"
-		-- 	debugger_path = "/Users/luu/manual_builds/vscode-js-debug/", -- Path to vscode-js-debug installation.
-		-- 	-- debugger_cmd = { "js-debug-adapter" }, -- Command to use to launch the debug server. Takes precedence over `node_path` and `debugger_path`.
-		-- 	adapters = { "pwa-node", "pwa-chrome", "pwa-msedge", "node-terminal", "pwa-extensionHost" }, -- which adapters to register in nvim-dap
-		-- 	-- log_file_path = "(stdpath cache)/dap_vscode_js.log" -- Path for file logging
-		-- 	-- log_file_level = false -- Logging level for output to file. Set to false to disable file logging.
-		-- 	-- log_console_level = vim.log.levels.ERROR -- Logging level for output to console. Set to false to disable console output.
-		-- })
-		--
-		-- for _, language in ipairs({ "typescript", "javascript" }) do
-		-- 	dap.configurations[language] = {
-		-- 		{
-		-- 			type = "node-terminal",
-		-- 			request = "launch",
-		-- 			name = "Launch via npm run dev",
-		-- 			cwd = vim.fn.getcwd(),
-		-- 			runtimeExecutable = "npm",
-		-- 			runtimeArgs = { "run", "dev" },
-		-- 			skipFiles = { "<node_internals>/**" },
-		-- 			console = "integratedTerminal", -- or "externalTerminal"
-		-- 		},
-		-- 	}
-		-- end
 	end,
 }
