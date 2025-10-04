1e52008dc364d7244ccb1bcd7e366f2ccf9163b8
Tri D Luu <91616223+triluu03@users.noreply.github.com>
2025-10-04 22:20:38 +0300

    nvim: neo-tree document symbols

--- nvim/lua/kickstart/plugins/neo-tree.lua
+++ nvim/lua/kickstart/plugins/neo-tree.lua
@@ -2,24 +2,32 @@
 -- https://github.com/nvim-neo-tree/neo-tree.nvim
 
 return {
-  'nvim-neo-tree/neo-tree.nvim',
-  version = '*',
-  dependencies = {
-    'nvim-lua/plenary.nvim',
-    'nvim-tree/nvim-web-devicons', -- not strictly required, but recommended
-    'MunifTanjim/nui.nvim',
-  },
-  lazy = false,
-  keys = {
-    { '\\', ':Neotree reveal<CR>', desc = 'NeoTree reveal', silent = true },
-  },
-  opts = {
-    filesystem = {
-      window = {
-        mappings = {
-          ['\\'] = 'close_window',
-        },
-      },
-    },
-  },
+	"nvim-neo-tree/neo-tree.nvim",
+	version = "*",
+	dependencies = {
+		"nvim-lua/plenary.nvim",
+		"nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
+		"MunifTanjim/nui.nvim",
+	},
+	lazy = false,
+	keys = {
+		{ "\\", ":Neotree reveal<CR>",           desc = "NeoTree reveal",           silent = true },
+		{ "|",  ":Neotree document_symbols<CR>", desc = "NeoTree document symbols", silent = true },
+	},
+	opts = {
+		sources = {
+			"filesystem",
+			"buffers",
+			-- "git_status",
+			"last",
+			"document_symbols",
+		},
+		filesystem = {
+			window = {
+				mappings = {
+					["\\"] = "close_window",
+				},
+			},
+		},
+	},
 }
