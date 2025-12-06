3fb6aff6a41827ff3c2c37b691cdc9b846e59be7
Tri D Luu <91616223+triluu03@users.noreply.github.com>
2025-12-06 09:50:22 +0200

    nvim: change the pickers of lsp_definitions of telescope

--- nvim/lua/kickstart/plugins/telescope.lua
+++ nvim/lua/kickstart/plugins/telescope.lua
@@ -26,7 +26,7 @@ return { -- Fuzzy Finder (files, lsp, etc)
 		{ "nvim-telescope/telescope-ui-select.nvim" },
 
 		-- Useful for getting pretty icons, but requires a Nerd Font.
-		{ "nvim-tree/nvim-web-devicons",            enabled = vim.g.have_nerd_font },
+		{ "nvim-tree/nvim-web-devicons", enabled = vim.g.have_nerd_font },
 	},
 	config = function()
 		-- Telescope is a fuzzy finder that comes with a lot of different things that
@@ -59,7 +59,7 @@ return { -- Fuzzy Finder (files, lsp, etc)
 			--     i = { ['<c-enter>'] = 'to_fuzzy_refine' },
 			--   },
 			-- },
-			-- pickers = {}
+			pickers = { lsp_definitions = { jump_type = "vsplit" } },
 			extensions = {
 				["ui-select"] = {
 					require("telescope.themes").get_dropdown(),
